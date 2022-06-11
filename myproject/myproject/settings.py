bb57f336fd7d789bc9295fa6ffa2f2b3c51e4de2
TcarMolod <93925126+TcarMolod@users.noreply.github.com>
2022-06-11 21:47:20 +0500

    lection 3.2

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp',
+    'backend',
+    'frontend',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL='/login/'
